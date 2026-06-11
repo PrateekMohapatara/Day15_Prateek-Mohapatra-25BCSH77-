a88b92bd86d1bfbce82afb24d9c5a960e894ddf4
Prateek Mohapatra 25BCSH77 <cse.25bcsh77@silicon.ac.in>
2026-06-11 19:26:32 +0530

    Update Online_Food_Order_Notification.java

--- Multithreading/Online_Food_Order_Notification.java
+++ Multithreading/Online_Food_Order_Notification.java
@@ -92,3 +92,49 @@ Main Method
     Call placeOrder()
 
 */
+package day15_progs;
+
+interface NotificationService {
+    void sendMessage();
+}
+
+class EmailNotification implements NotificationService {
+    public void sendMessage() {
+        System.out.println("Email Notification Sent");
+    }
+}
+
+class SMSNotification implements NotificationService {
+    public void sendMessage() {
+        System.out.println("SMS Notification Sent");
+    }
+}
+
+class OrderService {
+    NotificationService notificationService;
+
+    OrderService(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    void placeOrder() {
+        System.out.println("Order Confirmed");
+        notificationService.sendMessage();
+    }
+}
+
+public class Online_Food_Order_Notification {
+
+    public static void main(String[] args) {
+
+        NotificationService email = new EmailNotification();
+        OrderService order1 = new OrderService(email);
+        order1.placeOrder();
+
+        System.out.println();
+
+        NotificationService sms = new SMSNotification();
+        OrderService order2 = new OrderService(sms);
+        order2.placeOrder();
+    }
+}
