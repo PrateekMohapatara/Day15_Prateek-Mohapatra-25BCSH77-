a7001e3f6652dba96bd3389cfdbb633eef68286c
Prateek Mohapatra 25BCSH77 <cse.25bcsh77@silicon.ac.in>
2026-06-11 19:25:29 +0530

    Update Classroom_attendance_system.java

--- Multithreading/Classroom_attendance_system.java
+++ Multithreading/Classroom_attendance_system.java
@@ -89,3 +89,72 @@ Main Method
 Thread Coordination
 Synchronization
 */
+package day15_progs;
+import java.util.*;
+class system{
+	boolean isStarted =false;
+	synchronized void Wait_attendance() {
+		while(!isStarted) {
+			System.out.println(Thread.currentThread().getName() + " Waiting...");
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}		
+		}
+	
+		System.out.println(Thread.currentThread().getName() +" marked present");
+	}
+				
+	synchronized void start_attendance(){
+		
+		System.out.println("Teacher Started Attendance");
+		isStarted =true;
+		notifyAll();	
+		
+	}
+			
+	}
+class TeacherThread extends Thread{
+	system system;
+	TeacherThread(system system){
+		this.system=system;
+	}
+	public void run() {
+		try {
+			Thread.sleep(2000);
+		}
+		catch(Exception e) {
+			
+		}
+		system.start_attendance();
+	}
+}
+class StudentThread extends Thread {
+	system system;
+	StudentThread(system system){
+		this.system=system;
+	}
+	public void run() {
+		system.Wait_attendance();
+	}
+}
+public class Classroom_attendance_system {
+
+	public static void main(String[] args) {
+
+	    system system = new system();
+
+	    StudentThread s1 = new StudentThread(system);
+	    StudentThread s2 = new StudentThread(system);
+	    StudentThread s3 = new StudentThread(system);
+
+	    TeacherThread t1 = new TeacherThread(system);
+
+	    s1.start();
+	    s2.start();
+	    s3.start();
+
+	    t1.start();
+	}
+}
